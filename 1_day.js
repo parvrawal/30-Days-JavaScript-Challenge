a83d81ae66a2b5800e43b2fd7d3457429ebe756f
Parv Rawal <167647084+parvrawal@users.noreply.github.com>
2024-07-13 23:34:55 +0530

    Add files via upload

---
+++ 1_day.js
@@ -0,0 +1,51 @@
+console.log("I'm working");
+
+// Activity 1:- 
+var totalPrice = 240;
+console.log(totalPrice);
+
+let shopeName = "Raju Masale Wala";
+console.log(shopeName);
+
+// Activity 2:-
+const online =  true;
+
+// Activity 3:-
+
+const  myNumber = 34;
+const myName = "Luffy";
+let myAvailablity = false;
+const childhoodHeros = ["Spider Man", "Batman", "Superman"];
+const contactDetails = {
+   
+        Email: "example@google.com",
+        Linkdin: "example_test",
+        Twitter: "example_test",
+
+    }
+ 
+console.log(`The value is ${myNumber} and the data type is ${typeof myNumber}` );
+console.log(`The value is ${myName} and the data type is ${typeof myName}` );
+console.log(`The value is ${myAvailablity} and the data type is ${typeof myAvailablity}` );
+console.log(`The value is ${childhoodHeros} and the data type is ${typeof childhoodHeros}` );
+console.log(`The value is ${contactDetails} and the data type is ${typeof contactDetails}` );
+
+
+// Activity 4:-
+
+let favHero = "Batman";
+console.log(favHero);
+favHero = "Superman";
+console.log(favHero);
+
+// let allows reassignment 
+
+// Activity 5:- 
+const favCartoon = "Ben 10";
+console.log(favCartoon);
+favCartoon = "Scooby-Doo";
+console.log(favCartoon);
+
+// does not allow reassignment 
+
+
